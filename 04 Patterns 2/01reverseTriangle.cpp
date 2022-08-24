59d4a5f08c67a0b7c4910acf185e3235052a3a4d
Aditya Patil <78469187+aditya30x21@users.noreply.github.com>
2022-08-24 23:08:20 +0530

    Add files via upload

---
+++ 04 Patterns 2/01reverseTriangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int currRow = 1;
+    while (currRow <= n)
+    {
+        int spaces = 1;
+        while (spaces <= n - currRow)
+        {
+            cout << " ";
+            spaces++;
+        }
+
+        int currCol = 1;
+        while (currCol <= currRow)
+        {
+            cout << "*";
+            currCol++;
+        }
+        currRow++;
+        cout << endl;
+    }
+}
