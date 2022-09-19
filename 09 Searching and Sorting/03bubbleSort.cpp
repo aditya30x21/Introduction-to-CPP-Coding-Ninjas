57d62b7f25f79ddd3022e495c81b03ecf8c4eb39
Aditya Patil <78469187+aditya30x21@users.noreply.github.com>
2022-09-20 00:24:33 +0530

    Add files via upload

---
+++ 09 Searching and Sorting/03bubbleSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+// Time - O(n^2)
+// Space - O(1)
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+            // cout << i<<endl;     //TO SEE WHICH ITERATION IS GOING
+            // printArray(arr, n);  //FOR STEP TO STEP WORKING
+        }
+        //  cout<<endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubbleSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
